cd19c496a7dbf5229c6d9db8c6afa75871d8fe0f
tom spaans <tom.spaans@saintmichelannecy.fr>
2024-11-25 11:59:07 +0100

    api

--- API/MainWindow.xaml.cs
+++ API/MainWindow.xaml.cs
@@ -8,6 +8,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Tweetinvi;
+using System.Windows.Media.Animation;
+using Tweetinvi.Models;
+using Tweetinvi.Core.Models;
 
 namespace API
 {
@@ -19,6 +25,17 @@ namespace API
         public MainWindow()
         {
             InitializeComponent();
+
+            Auth.SetUserCredentials("qsc5SXJkZeEK8fbL7Iqjk6ACk", "fDSMeRCANfL7Gio3k3NiOqEnksgl4pBvPKJpwqcTrsOS1m81Sw", "1860986408858435584-D0lapfTYWU5ecw7eyiSzIXUZ7PN3gR", "nmyPIMS278ciFAbdiiuBTPS9yYv8xM7Hg7fCWjahNJYFu");
+
+            // Récupère les 10 derniers tweets de l'utilisateur spécifié
+            var userTweets = User.GetUserFromScreenName("nomUtilisateur").Timeline.GetHomeTimeline(10);
+
+            // Affiche les textes des tweets dans la ListBox
+            foreach (var tweet in userTweets)
+            {
+                tweetListBox.Items.Add(tweet.FullText);
+            }
         }
     }
 }
